33fee070883d846da9a7e86cfcd01fd0bc931149
KWATRA55 <shashwatkwatra55@gmail.com>
2023-03-17 22:46:50 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            shashwat kwatra&nbsp;
+            Shashwat Kwatra &nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
@@ -67,7 +67,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              shashwat <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
