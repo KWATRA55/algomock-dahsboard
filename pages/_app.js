ac7370d9fd8532bcdb56e183440c848a8fb752fd
KWATRA55 <shashwatkwatra55@gmail.com>
2023-03-20 15:24:24 +0530

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { useState } from 'react';
+import { AuthProvider } from '@/context/AuthProvider';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [value, setValue] = useState([]);
+  return(
+      <AuthProvider>
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+      </AuthProvider>
+  )
 }
