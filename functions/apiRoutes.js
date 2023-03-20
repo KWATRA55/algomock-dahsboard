0f2aa69b053364ea212b80b26c1728fdd3f6ad35
KWATRA55 <shashwatkwatra55@gmail.com>
2023-03-20 12:46:50 +0530

    first commit

--- functions/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-export const baseUrl = 'https://stage.algomock.in/v1/';
-
-export const header = {'Content-Type' : 'application/json'}
-
-export const url = {
-    'auth.register': '/auth/register',
-	'auth.login': '/auth/login',
-}
