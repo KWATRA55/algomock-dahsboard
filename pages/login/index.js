b1d682c2d48c150382b626c239e0f4fc9098a78d
KWATRA55 <shashwatkwatra55@gmail.com>
2023-03-31 00:52:45 +0530

    first commit

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,306 +1,314 @@
 import {
-    Box,
-    Flex,
-    Stack,
-    Heading,
-    Text,
-    Container,
-    Input,
-    Button,
-    SimpleGrid,
-    Avatar,
-    AvatarGroup,
-    useBreakpointValue,
-    IconProps,
-    Icon,
-  } from '@chakra-ui/react';
-  import Router from 'next/router'
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Container,
+  Input,
+  Button,
+  SimpleGrid,
+  Avatar,
+  AvatarGroup,
+  useBreakpointValue,
+  IconProps,
+  Icon,
+} from '@chakra-ui/react';
+import Router from 'next/router'
 import { useState, useContext, useEffect, useRef } from 'react';
 import { login } from '@/functions/request';
 import AuthContext from '@/context/AuthProvider';
-  
-  const avatars = [
-    {
-      name: 'Ryan Florence',
-      url: 'https://bit.ly/ryan-florence',
-    },
-    {
-      name: 'Segun Adebayo',
-      url: 'https://bit.ly/sage-adebayo',
-    },
-    {
-      name: 'Kent Dodds',
-      url: 'https://bit.ly/kent-c-dodds',
-    },
-    {
-      name: 'Prosper Otemuyiwa',
-      url: 'https://bit.ly/prosper-baba',
-    },
-    {
-      name: 'Christian Nwamba',
-      url: 'https://bit.ly/code-beast',
-    },
-  ];
-  
+import { setLocalStorage } from '@/functions/dashboardFunctions';
 
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  export default function JoinOurTeam() {
-
-    const {auth, setAuth} = useContext(AuthContext);
-
-    const errRef = useRef();
-
-    const [hasError, setHasError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [data, setData] = useState([]);
-
-    const [success, setSuccess] = useState(false);
- 
+const avatars = [
+  {
+    name: 'Ryan Florence',
+    url: 'https://bit.ly/ryan-florence',
+  },
+  {
+    name: 'Segun Adebayo',
+    url: 'https://bit.ly/sage-adebayo',
+  },
+  {
+    name: 'Kent Dodds',
+    url: 'https://bit.ly/kent-c-dodds',
+  },
+  {
+    name: 'Prosper Otemuyiwa',
+    url: 'https://bit.ly/prosper-baba',
+  },
+  {
+    name: 'Christian Nwamba',
+    url: 'https://bit.ly/code-beast',
+  },
+];
 
 
-    const handleClick = async () => {
-      try{
-        let json = await login(email, password);
-          if(json?.code > 202){
-            setHasError(true);
-            setErrorMessage(json?.message);
-          }
-          console.log('json response : ',json);
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-          const accessToken = json?.data?.tokens?.access?.token;
+export default function JoinOurTeam() {
 
-          setAuth({email, password, accessToken});
-          console.log('auth : ', auth);
-          setData(json);
+  const {auth, setAuth} = useContext(AuthContext);
 
-          console.log('token : ', json?.data?.tokens?.access?.token);
-          sessionStorage.setItem("key", json?.data?.tokens?.access?.token);
-          Router.push(`/dashboard`); 
-        }
-        catch(err){
+  const errRef = useRef();
+
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [data, setData] = useState([]);
+
+  const [success, setSuccess] = useState(false);
+
+
+  const handleClick = async () => {
+    try{
+      let json = await login(email, password);
+        if(json?.code > 202){
           setHasError(true);
-          console.log(err);
+          setErrorMessage(json?.message);
         }
-  
-        // var requestOptions = {
-        //   method: 'POST',
-        //   headers: myHeaders,
-        //   body: raw,
-        //   redirect: 'follow'
-        // };
+        console.log('json response : ',json);
 
+        const accessToken = json?.data?.tokens?.access?.token;
+        const refreshToken = json?.data?.tokens?.refresh?.token;
+        const userId = json?.data?.user?.id;
 
-      // try{
-      //   const res = await fetch("https://stage.algomock.in/v1//auth/login", requestOptions)
-      //   const json = await res.json();
-      //   if(json.code > 202){
-      //     setHasError(true);
-      //     setErrorMessage(json.message);
-      //   }
-      //   console.log(json)
-      //   setData(json);
+        setAuth({email, password, accessToken, refreshToken, userId});
+        setData(json);
 
-      //   setValue(Object.values(json));
-      
-      //   console.log(json.tokens.access.token);
-      //   sessionStorage.setItem("key", json.tokens.access.token);
-      //   Router.push(`/dashboard/${json.user.id}`); 
-      // }
-      // catch(err){
-      //   setHasError(true);
-      //   console.log(err);
-      // }
+        console.log('token : ', json?.data?.tokens?.access?.token);
+        console.log('user id : ', userId);
+
+        setLocalStorage('accessToken', accessToken);
+        setLocalStorage('refreshToken', refreshToken);
+        setLocalStorage('userId', userId);
+
+        // localStorage.setItem("accessToken", json?.data?.tokens?.access?.token);
+        // localStorage.setItem("userId", json?.data?.user?.id);
+        Router.push(`/dashboard`); 
+      }
+      catch(err){
+        setHasError(true);
+        console.log(err);
       }
 
-      useEffect(() => {
-        setErrorMessage('');
-      }, [email, password]);
+      // var requestOptions = {
+      //   method: 'POST',
+      //   headers: myHeaders,
+      //   body: raw,
+      //   redirect: 'follow'
+      // };
 
-    return (
-      <Box position={'relative'}>
-        <Container
-          as={SimpleGrid}
-          maxW={'7xl'}
-          columns={{ base: 1, md: 2 }}
-          spacing={{ base: 10, lg: 32 }}
-          py={{ base: 10, sm: 20, lg: 32 }}>
-          <Stack spacing={{ base: 10, md: 20 }}>
+
+    // try{
+    //   const res = await fetch("https://stage.algomock.in/v1//auth/login", requestOptions)
+    //   const json = await res.json();
+    //   if(json.code > 202){
+    //     setHasError(true);
+    //     setErrorMessage(json.message);
+    //   }
+    //   console.log(json)
+    //   setData(json);
+
+    //   setValue(Object.values(json));
+    
+    //   console.log(json.tokens.access.token);
+    //   sessionStorage.setItem("key", json.tokens.access.token);
+    //   Router.push(`/dashboard/${json.user.id}`); 
+    // }
+    // catch(err){
+    //   setHasError(true);
+    //   console.log(err);
+    // }
+    }
+
+    useEffect(() => {
+      setErrorMessage('');
+    }, [email, password]);
+
+  return (
+    <Box position={'relative'}>
+      <Container
+        as={SimpleGrid}
+        maxW={'7xl'}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 10, lg: 32 }}
+        py={{ base: 10, sm: 20, lg: 32 }}>
+        <Stack spacing={{ base: 10, md: 20 }}>
+          <Heading
+            lineHeight={1.1}
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
+            Senior web designers{' '}
+            <Text
+              as={'span'}
+              bgGradient="linear(to-r, red.400,pink.400)"
+              bgClip="text">
+              &
+            </Text>{' '}
+            Full-Stack Developers
+          </Heading>
+          <Stack direction={'row'} spacing={4} align={'center'}>
+            <AvatarGroup>
+              {avatars.map((avatar) => (
+                <Avatar
+                  key={avatar.name}
+                  name={avatar.name}
+                  src={avatar.url}
+                  size={useBreakpointValue({ base: 'md', md: 'lg' })}
+                  position={'relative'}
+                  zIndex={2}
+                  _before={{
+                    content: '""',
+                    width: 'full',
+                    height: 'full',
+                    rounded: 'full',
+                    transform: 'scale(1.125)',
+                    bgGradient: 'linear(to-bl, red.400,pink.400)',
+                    position: 'absolute',
+                    zIndex: -1,
+                    top: 0,
+                    left: 0,
+                  }}
+                />
+              ))}
+            </AvatarGroup>
+            <Text fontFamily={'heading'} fontSize={{ base: '4xl', md: '6xl' }}>
+              +
+            </Text>
+            <Flex
+              align={'center'}
+              justify={'center'}
+              fontFamily={'heading'}
+              fontSize={{ base: 'sm', md: 'lg' }}
+              bg={'gray.800'}
+              color={'white'}
+              rounded={'full'}
+              minWidth={useBreakpointValue({ base: '44px', md: '60px' })}
+              minHeight={useBreakpointValue({ base: '44px', md: '60px' })}
+              position={'relative'}
+              _before={{
+                content: '""',
+                width: 'full',
+                height: 'full',
+                rounded: 'full',
+                transform: 'scale(1.125)',
+                bgGradient: 'linear(to-bl, orange.400,yellow.400)',
+                position: 'absolute',
+                zIndex: -1,
+                top: 0,
+                left: 0,
+              }}>
+              YOU
+            </Flex>
+          </Stack>
+        </Stack>
+        <Stack
+          bg={'gray.50'}
+          rounded={'xl'}
+          p={{ base: 4, sm: 6, md: 8 }}
+          spacing={{ base: 8 }}
+          maxW={{ lg: 'lg' }}>
+          <Stack spacing={4}>
             <Heading
+              color={'gray.800'}
               lineHeight={1.1}
-              fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
-              Senior web designers{' '}
+              fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+              Login
               <Text
                 as={'span'}
                 bgGradient="linear(to-r, red.400,pink.400)"
                 bgClip="text">
-                &
-              </Text>{' '}
-              Full-Stack Developers
+                !
+              </Text>
             </Heading>
-            <Stack direction={'row'} spacing={4} align={'center'}>
-              <AvatarGroup>
-                {avatars.map((avatar) => (
-                  <Avatar
-                    key={avatar.name}
-                    name={avatar.name}
-                    src={avatar.url}
-                    size={useBreakpointValue({ base: 'md', md: 'lg' })}
-                    position={'relative'}
-                    zIndex={2}
-                    _before={{
-                      content: '""',
-                      width: 'full',
-                      height: 'full',
-                      rounded: 'full',
-                      transform: 'scale(1.125)',
-                      bgGradient: 'linear(to-bl, red.400,pink.400)',
-                      position: 'absolute',
-                      zIndex: -1,
-                      top: 0,
-                      left: 0,
-                    }}
-                  />
-                ))}
-              </AvatarGroup>
-              <Text fontFamily={'heading'} fontSize={{ base: '4xl', md: '6xl' }}>
-                +
-              </Text>
-              <Flex
-                align={'center'}
-                justify={'center'}
-                fontFamily={'heading'}
-                fontSize={{ base: 'sm', md: 'lg' }}
-                bg={'gray.800'}
-                color={'white'}
-                rounded={'full'}
-                minWidth={useBreakpointValue({ base: '44px', md: '60px' })}
-                minHeight={useBreakpointValue({ base: '44px', md: '60px' })}
-                position={'relative'}
-                _before={{
-                  content: '""',
-                  width: 'full',
-                  height: 'full',
-                  rounded: 'full',
-                  transform: 'scale(1.125)',
-                  bgGradient: 'linear(to-bl, orange.400,yellow.400)',
-                  position: 'absolute',
-                  zIndex: -1,
-                  top: 0,
-                  left: 0,
-                }}>
-                YOU
-              </Flex>
-            </Stack>
+            <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
+             Login If you already have an account
+            </Text>
           </Stack>
-          <Stack
-            bg={'gray.50'}
-            rounded={'xl'}
-            p={{ base: 4, sm: 6, md: 8 }}
-            spacing={{ base: 8 }}
-            maxW={{ lg: 'lg' }}>
+          <Box as={'form'} mt={10}>
             <Stack spacing={4}>
-              <Heading
-                color={'gray.800'}
-                lineHeight={1.1}
-                fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
-                Login
-                <Text
-                  as={'span'}
-                  bgGradient="linear(to-r, red.400,pink.400)"
-                  bgClip="text">
-                  !
-                </Text>
-              </Heading>
-              <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
-               Login If you already have an account
-              </Text>
+              <Input
+                placeholder="Email"
+                bg={'gray.100'}
+                border={0}
+                color={'gray.500'}
+                _placeholder={{
+                  color: 'gray.500',
+                }}
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <Input
+                placeholder="Password"
+                type='password'
+                bg={'gray.100'}
+                border={0}
+                color={'gray.500'}
+                _placeholder={{
+                  color: 'gray.500',
+                }}
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              
             </Stack>
-            <Box as={'form'} mt={10}>
-              <Stack spacing={4}>
-                <Input
-                  placeholder="Email"
-                  bg={'gray.100'}
-                  border={0}
-                  color={'gray.500'}
-                  _placeholder={{
-                    color: 'gray.500',
-                  }}
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                />
-                <Input
-                  placeholder="Password"
-                  type='password'
-                  bg={'gray.100'}
-                  border={0}
-                  color={'gray.500'}
-                  _placeholder={{
-                    color: 'gray.500',
-                  }}
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                />
-                
-              </Stack>
-              <Button
-                fontFamily={'heading'}
-                mt={8}
-                w={'full'}
+            <Button
+              fontFamily={'heading'}
+              mt={8}
+              w={'full'}
+              bgGradient="linear(to-r, red.400,pink.400)"
+              color={'white'}
+              onClick={handleClick}
+              _hover={{
+                bgGradient: 'linear(to-r, red.400,pink.400)',
+                boxShadow: 'xl',
+              }}>
+              Submit
+            </Button>
+            {hasError ? <Text
+                as={'span'}
                 bgGradient="linear(to-r, red.400,pink.400)"
-                color={'white'}
-                onClick={handleClick}
-                _hover={{
-                  bgGradient: 'linear(to-r, red.400,pink.400)',
-                  boxShadow: 'xl',
-                }}>
-                Submit
-              </Button>
-              {hasError ? <Text
-                  as={'span'}
-                  bgGradient="linear(to-r, red.400,pink.400)"
-                  bgClip="text">
-                  {errorMessage}
-                </Text> : null}
-
-              <p ref={errRef} className={errorMessage ? "errmsg" : "offscreen"} aria-live="assertive">
+                bgClip="text">
                 {errorMessage}
-              </p>
-            </Box>
-            form
-          </Stack>
-        </Container>
-        <Blur
-          position={'absolute'}
-          top={-10}
-          left={-10}
-          style={{ filter: 'blur(70px)' }}
-        />
-      </Box>
-    );
-  }
-  
-  export const Blur = (props) => {
-    return (
-      <Icon
-        width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
-        zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-        height="560px"
-        viewBox="0 0 528 560"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}>
-        <circle cx="71" cy="61" r="111" fill="#F56565" />
-        <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-        <circle cy="291" r="139" fill="#ED64A6" />
-        <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-        <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-        <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-        <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
-      </Icon>
-    );
-  };
+              </Text> : null}
+
+            <p ref={errRef} className={errorMessage ? "errmsg" : "offscreen"} aria-live="assertive">
+              {errorMessage}
+            </p>
+          </Box>
+          form
+        </Stack>
+      </Container>
+      <Blur
+        position={'absolute'}
+        top={-10}
+        left={-10}
+        style={{ filter: 'blur(70px)' }}
+      />
+    </Box>
+  );
+}
+
+export const Blur = (props) => {
+  return (
+    <Icon
+      width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+      zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
+      height="560px"
+      viewBox="0 0 528 560"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <circle cx="71" cy="61" r="111" fill="#F56565" />
+      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+      <circle cy="291" r="139" fill="#ED64A6" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+    </Icon>
+  );
+};
